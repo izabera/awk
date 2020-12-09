59d07a34c2e08de4e5352f80ab32ae126c1beffb
Isabella Bosia <izaberina@gmail.com>
2020-12-09 09:06:00 +0000

    add field splitter

--- awk.c
+++ awk.c
@@ -56,11 +56,30 @@ struct var *var(struct query query) {
 
 end:
     freeentry(entry);
-    return v ? *v : v;
+    return v ? *v : NULL;
 }
 
 
 
+struct vector {
+    void *data;
+    size_t alloc;
+    size_t used;
+};
+
+size_t regexsplit(regex_t *regex, const char *str, struct vector *delims) {
+    for (size_t i = 0; ; i++) {
+        if (delims->used + sizeof(regmatch_t) > delims->alloc)
+        {
+            delims->alloc = delims->used * 2 + sizeof(regmatch_t);
+            delims->data = realloc(delims->data, delims->alloc);
+        }
+        if (regexec(regex, str, 1, (regmatch_t *)delims->data + i, 0))
+            break;
+        delims->used += sizeof(regmatch_t);
+        str += ((regmatch_t *)delims->data + i)->rm_eo;
+    }
+}
 
 
 int main(/* int argc, char *argv[] */) {
@@ -68,6 +87,8 @@ int main(/* int argc, char *argv[] */) {
 #define var(...) var((struct query) { __VA_ARGS__, .search = tsearch })
 #define value(...) ((char *)(var(__VA_ARGS__)->value))
 #define in(...) (var)((struct query) { __VA_ARGS__, .search = (void *)tfind })
+
+    puts("BEGIN {");
 
     puts("foo = \"meow\"");
     var("foo", .value = "meow");
@@ -81,18 +102,53 @@ int main(/* int argc, char *argv[] */) {
     puts("baz[\"asdf\"] = \"world\"");
     var("baz", "asdf", "world");
 
-    puts("");
+
+#define printawk(code, result) printf("%-50s # %s\n", code, result)
+    printawk("print foo", value("foo"));
+    printawk("print foo = \"awk\"", value("foo", .value = "awk"));
+
+    printawk("print bar", value("bar"));
+    printawk("print baz[42]", value("baz", "42"));
+    printawk("print baz[\"asdf\"]", value("baz", "asdf"));
+
+    printawk("print 42 in baz", in("baz", "42") ? "1" : "0");
+    printawk("print \"fffff\" in baz", in("baz", "fffff") ? "1" : "0");
 
 
-    printf("%-30s -> %s\n", "print foo", value("foo"));
-    printf("%-30s -> %s\n", "print foo = \"awk\"", value("foo", .value = "awk"));
+    char *string = "12345:67-89meow000";
+    char *delims = "[^0-9]+";
 
-    printf("%-30s -> %s\n", "print bar", value("bar"));
-    printf("%-30s -> %s\n", "print baz[42]", value("baz", "42"));
-    printf("%-30s -> %s\n", "print baz[\"asdf\"]", value("baz", "asdf"));
+    printf("split(\"%s\", array, /%s/)\n", string, delims);
 
-    printf("%-30s -> %s\n", "42 in baz", in("baz", "42") ? "true" : "false");
-    printf("%-30s -> %s\n", "\"fffff\" in baz", in("baz", "fffff") ? "true" : "false");
+    regex_t regex;
+    regcomp(&regex, delims, REG_EXTENDED);
+    struct vector v = { };
+    regexsplit(&regex, string, &v);
+    regmatch_t *matches = v.data;
+
+    int i;
+    for (i = 0; i < v.used/sizeof *matches; i++) {
+        char idx[20];
+        sprintf(idx, "%d", i + 1);
+
+        char value[matches[i].rm_so + 1];
+        sprintf(value, "%.*s", matches[i].rm_so, string);
+
+        var("array", idx, value);
+        string += matches[i].rm_eo;
+    }
+    char idx[20];
+    sprintf(idx, "%d", i + 1);
+    var("array", idx, string);
+
+    printawk("print array[1]", value("array", "1"));
+    printawk("print array[2]", value("array", "2"));
+    printawk("print array[3]", value("array", "3"));
+    printawk("print array[4]", value("array", "4"));
+
+
+    puts("}");
+
     /*
     char *line = NULL;
     size_t n = 0;
